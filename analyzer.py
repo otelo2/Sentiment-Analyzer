cfb86753c67da1b0ae8639a5ac07d5ea71a9a90b
Otelo2 <josean2009@live.com.mx>
2020-10-16 15:57:43 -0500

    Add stream collection

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,9 @@
+#From https://marcobonzanini.com/2015/03/02/mining-twitter-data-with-python-part-1/
+#http://docs.tweepy.org/en/latest/streaming_how_to.html
 import tweepy
 from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
 from keys import *
 
 auth = OAuthHandler(apiKey, apiKeySecret)
@@ -7,5 +11,28 @@ auth.set_access_token(accessToken, accessTokenSecret)
 
 api = tweepy.API(auth)
 
-for status in tweepy.Cursor(api.home_timeline).items(3):
-    print(status.text)
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error onData: %s" % str(e))
+        return True
+
+    def on_status(self, status):
+        return super().on_status(status)
+
+    def on_error(self, status_code):
+        print(status_code)
+        if status_code == 420:
+            #Returning false disconects from stream
+            return False
+        return True
+
+
+myStreamListener = MyStreamListener()
+twitterStream = tweepy.Stream(auth, myStreamListener)
+twitterStream.filter(track=['AMLO'], languages=['es', 'en'])
